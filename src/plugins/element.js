07cfbd068da62e26eaba47715d289d91c677737d
zengjin07 <zengjin07@countrygarden.com.cn>
2019-12-05 17:50:38 +0800

    完成订单开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,4 +2,10 @@ import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
+
+Vue.use(Timeline)
+Vue.use(TimelineItem)
+
 Vue.use(Element)
