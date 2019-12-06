62c7132334a491e73d7b63dec39a48f9294a58f3
zengjin07 <zengjin07@countrygarden.com.cn>
2019-12-06 10:51:29 +0800

    完成报表的开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params'
 import GoodsList from '../components/goods/List'
 import AddGoods from '../components/goods/Add'
 import Order from '../components/order/Order'
+import Reports from '../components/reports/Reports'
 
 Vue.use(VueRouter)
 
@@ -30,7 +31,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: GoodsList },
       { path: '/goods/add', component: AddGoods },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Reports }
     ]
   }
 ]
